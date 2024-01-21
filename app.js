ac05b6b70790b4e96459a3d9b1817657bf7dc5d0
unknown <vaishnavi.salunkhe@kalvium.community>
2024-01-21 11:25:50 +0530

    made changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const buttons = document.getElementsByClassName("button")
-const cartValue = document.getElementById("cart-value")
-const cart = document.getElementById("cart")
+const button = document.getElementsByClassName("button");
+const cartValue = document.getElementById("cart-value");
+const cart = document.getElementById("cart");
 
 var items = [
     {
@@ -89,36 +89,59 @@ var items = [
     },
   ];
 
-for(let i=0;i<buttons.length;i++){
-    buttons[i].addEventListener("click",()=>{
+  for(let i=0; i<button.length; i++ ){
+    button[i].addEventListener("click",()=>{
+
         items[i].quantity++;
-        updateCart()
+        updateCart();
     })
-}
+  }
+  function updateCart(){
+    let cart = 0;
 
-function updateCart(){
-    let cart =0;
-
-    for(let i=0;i<items.length;i++){
+    for(let i=0; i<items.length; i++){
         cart+=items[i].quantity;
     }
-    cartValue.innerHTML = cart;
-}
+    cartValue.innerHTML  = cart;
+  }
 
-let finalDollars = 0;
-let finalcents = 0;
+  var finalDollars = 0;
+  var finalCents =0;
 
-const updatePrice = ()=>{
-    let total = 0;
-
-    for (let i=0;i<items.length;i++){
-        total+=items[i].quantity*(items[i].dollars*100+items[i].cents);
+  function updatePrice(){
+    let total =0 ;
+    for(let i=0; i<items.length;i++){
+        total+=items[i].quantity*(items[i].dollars*100 + items[i].cents)
     }
 
     finalDollars = Math.floor(total/100);
-    finalcents = total % 100
-}
+    finalCents = total%100;
+  }
+  var updatewhatsapplink = "https://api.whatsapp.com/send?phone=919000000000&text=Order%20details"
+   
+  function whatsapplink(){
+    for(let i=0; i<items.length; i++){
+        if(items[i].quantity !=0){
+            whatsapplink+= "%0A" + items[i].name + "%0A" + items[i].quantity;
+        }
+    }
+    whatsapplink += "%0A" + "Total%0APrice" + finalDollars + "%0A" + finalCents;
+  }
 
-cart.onclick=()=>{
+  cart.onclick = () =>{
+    updateCart();
     updatePrice();
-}
+    // updatewhatsapplink();
+    
+
+    // window.open(whatsapplink, "_blank");
+
+    for(let i=0; i<items.length; i++){
+        if(items[i].quantity !=0){
+            console.log(
+                "Items Name: " +items[i].name + "Items Quantity " + items[i].quantity
+            )
+        }
+    }
+    console.log("The Total amount is "+ finalDollars+ "$ and "+ "The total cents are "+ finalCents+" cents")
+  }
